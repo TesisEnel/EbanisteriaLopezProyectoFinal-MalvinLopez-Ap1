d66f2a0ab3d612a355d9fd979644279fe05a671e
malvin <malvi@DESKTOP-57EMM99>
2025-08-02 06:23:36 +0200

    Crea clase cliente

---
+++ EbanisteriaLopezProyectoFinal/Components/Models/ClienteDto.cs
@@ -0,0 +1,13 @@
+﻿namespace EbanisteriaLopezProyectoFinal.Components.Models;
+
+public class ClienteDto
+{
+    public string Id { get; set; } = string.Empty;
+    public string? Email { get; set; }
+    public string? UserName { get; set; }
+    public string? PhoneNumber { get; set; }
+    public bool EmailConfirmed { get; set; }
+    public DateTimeOffset? LockoutEnd { get; set; }
+
+    public bool EstaBloqueado => LockoutEnd != null && LockoutEnd > DateTimeOffset.UtcNow;
+}
