f2c8143315840e57cb3738abff854479fd44d27b
macbookair <macbookair@MACBOOKAIRDCC0>
2025-08-02 11:51:00 -0400

    Crea tabla venta

---
+++ EbanisteriaLopezProyectoFinal/Components/Models/Venta.cs
@@ -0,0 +1,10 @@
+﻿using EbanisteriaLopezProyectoFinal.Components.Models;
+
+public class Venta
+{
+    public int VentaId { get; set; }
+    public string UsuarioId { get; set; }  // O la relación con un usuario
+    public DateTime Fecha { get; set; }
+    public decimal Total { get; set; }
+    public List<Producto> Productos { get; set; }  // Relación muchos a muchos
+}
