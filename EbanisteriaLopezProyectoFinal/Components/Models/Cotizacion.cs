2587819853f5df4c28382b2b95b4763f1e0202b5
malvin <malvi@DESKTOP-57EMM99>
2025-08-02 06:23:23 +0200

    Crea claase cotizacion

---
+++ EbanisteriaLopezProyectoFinal/Components/Models/Cotizacion.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EbanisteriaLopezProyectoFinal.Components.Models;
+
+public class Cotizacion
+{
+    [Key]
+    public int CotizacionId { get; set; }
+
+    [Required]
+    public string Nombre { get; set; } = string.Empty;
+
+    [Required]
+    public string Direccion { get; set; } = string.Empty;
+
+    [Required]
+    [Phone]
+    public string Telefono { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAddress]
+    public string Correo { get; set; } = string.Empty;
+
+    [Required]
+    public int ProductoId { get; set; }
+
+    [ForeignKey("ProductoId")]
+    public Producto? Producto { get; set; }
+    public bool EstaResuelto { get; set; } = false;
+
+
+    public DateTime FechaSolicitud { get; set; } = DateTime.Now;
+}
